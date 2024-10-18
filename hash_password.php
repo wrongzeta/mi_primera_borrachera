17e4dd47a8bf0b3b2eb8fc7c5679e5bec9e9af50
Santiago <santy8611@gmail.com>
2024-10-18 12:27:30 -0500

    visual

--- hash_password.php
+++ hash_password.php
@@ -8,3 +8,15 @@ $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 // Mostrar el hash
 echo $hashed_password;
 ?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi Primera Borrachera</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+</body>
+</html>
